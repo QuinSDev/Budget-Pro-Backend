e73cf69984f6a8418c750219af02b6980569aecd
QuinSDev <quindev97@gmail.com>
2023-10-19 16:41:48 -0500

    Se crea el paquete repository con la clase UserRepository que permitirá acceder a datos relacionados con el usuario en la base de datos.

---
+++ src/main/java/com/budget/budgetprobackend/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package com.budget.budgetprobackend.repository;
+
+import com.budget.budgetprobackend.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Este repositorio se utiliza para acceder a los datos relacionados con la
+ * entidad 'User' en la base de datos.
+ * Proporciona métodos par realizar operaciones de acceso a datos relacionados
+ * con los usuarios en la base de datos.
+ * 
+ * @author QuinSDev
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+    
+    /**
+     * Busca un usuario por su nombre de usuario.
+     * 
+     * @param userName El nombre de usuario del usuario que se desea buscar.
+     * @return Un objeto Optional que puede contener el usuario si se
+     * encuentra, o estar vacío si no se encuentra.
+     */
+    Optional<User> finByUserName(String userName);
+    
+}
