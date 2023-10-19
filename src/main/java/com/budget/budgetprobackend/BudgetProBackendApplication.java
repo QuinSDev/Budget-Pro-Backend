02e2eef68dcfa1715fbc9a6f18e8932903c0f3d8
QuinSDev <quindev97@gmail.com>
2023-10-18 20:11:51 -0500

    Se crea el backend de la aplicación web Budget-Pro

---
+++ src/main/java/com/budget/budgetprobackend/BudgetProBackendApplication.java
@@ -0,0 +1,13 @@
+package com.budget.budgetprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetProBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetProBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budget/budgetprobackend/BudgetProBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetprobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetProBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
