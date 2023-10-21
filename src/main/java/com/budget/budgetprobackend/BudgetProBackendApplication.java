d2b05fba942defd955f448b2ae1fb46fe9709ad0
QuinSDev <quindev97@gmail.com>
2023-10-21 09:50:35 -0500

    Se modifica la clase principal agregandole la anotación @ComponentScan para que configure adecuadamente todo los componentes y controladores en la aplicación.

--- src/main/java/com/budget/budgetprobackend/BudgetProBackendApplication.java
+++ src/main/java/com/budget/budgetprobackend/BudgetProBackendApplication.java
@@ -2,8 +2,24 @@ package com.budget.budgetprobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Clase principal de la aplicación Spring Boot
+ * Esta clase inicia la aplicación Spring Boot y configura la ubicación base para
+ * escanear componentes y controladores de la aplicación.
+ * 
+ * @SpringBootApplication indica que esta clase es la clase principal
+ *                        de la aplicación Spring Boot y configura automáticamente 
+ *                        la aplicación.
+ * @ComponentScan específica que la ubicación base para escaner componentes de
+ *                la aplicación, asegurando que Spring encuentre y configure 
+ *                todos los componentes y controladores necesarios.
+ * 
+ * @author QuinSDev
+ */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.budget.budgetprobackend")
 public class BudgetProBackendApplication {
 
 	public static void main(String[] args) {
